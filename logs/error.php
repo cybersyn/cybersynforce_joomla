0aaa0b2281f0871c334c4fafd2aa5b9ad397096c
cybersyn <cybersynforce@gmail.com>
2014-01-26 10:21:26 -0300

    Portal con Carrusel y Menu multilenguaje funcionando

---
+++ logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-01-23 12:58:04 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-01-23T12:58:04+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-01-23T12:58:07+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-01-23T12:58:11+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
